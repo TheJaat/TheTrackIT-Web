9fee81068a18a7f77487152745f0ecf702325038
TheJat <manishkl543@gmail.com>
2026-05-31 14:33:20 +0530

    fetch stats from backend and show in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,44 @@
-// src/app/dashboard/page.tsx
+'use client';
+
+import { useDashboard } from '@/hooks/use-dashboard';
 
 export default function DashboardPage() {
+  const {
+    data,
+    isLoading,
+  } = useDashboard();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      Dashboard
+      <h1>Dashboard</h1>
+
+      <h2>
+        Total Devices:
+        {' '}
+        {data.totalDevices}
+      </h2>
+
+      <h2>
+        Available:
+        {' '}
+        {data.availableDevices}
+      </h2>
+
+      <h2>
+        Allocated:
+        {' '}
+        {data.allocatedDevices}
+      </h2>
+
+      <h2>
+        Employees:
+        {' '}
+        {data.totalUsers}
+      </h2>
     </div>
   );
 }
